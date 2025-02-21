3219e67a36f722f930c59ef9f07f1295a9946c44
Lars Lehmann <lars@lars-lehmann.net>
2025-02-21 22:21:44 +0100

    fix: Remove default

--- internal/config/config.go
+++ internal/config/config.go
@@ -159,9 +159,6 @@ func setDefaults(conf *Config) {
 	if conf.UI.Theme == "" {
 		conf.UI.Theme = "light"
 	}
-	if conf.UI.Header.Logo == "" {
-		conf.UI.Header.Logo = "https://raw.githubusercontent.com/henrywhitaker3/prompage/refs/heads/main/internal/resources/static/icon.png"
-	}
 }
 
 func setDefaultQueryValues(q *Query) {
